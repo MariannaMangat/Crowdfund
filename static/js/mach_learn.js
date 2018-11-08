2fe15b6d2d991f399afff16a7986f363deb6d8ed
Laura <raynes.laura11@gmail.com>
2018-11-07 22:16:29 -0800

    add reset, fix bugs

--- static/js/mach_learn.js
+++ static/js/mach_learn.js
@@ -1,51 +1,53 @@
 $(document).ready(function() {
 
     d3.selectAll(".maincat > .list > li").on("click", function() {
+        d3.selectAll(".displayed").classed("displayed", false).classed("hidden", true);
+        
         switch (this.getAttribute("data-value")){
             case "Art":
-                d3.select(".artDiv").style("display", "inline-block");
+                d3.select(".artDiv").classed("displayed", true).classed("hidden", false);
                 break;
             case "Comics":
-                d3.select(".comicsDiv").style("display", "inline-block");
+                d3.select(".comicsDiv").classed("displayed", true).classed("hidden", false);
                 break;
             case "Crafts":
-                d3.select(".craftsDiv").style("display", "inline-block");
+                d3.select(".craftsDiv").classed("displayed", true).classed("hidden", false);
                 break;
             case "Dance":
-                d3.select(".danceDiv").style("display", "inline-block");
+                d3.select(".danceDiv").classed("displayed", true).classed("hidden", false);
                 break;
             case "Design":
-                d3.select(".designDiv").style("display", "inline-block");
+                d3.select(".designDiv").classed("displayed", true).classed("hidden", false);
                 break;
             case "Fashion":
-                d3.select(".fashionDiv").style("display", "inline-block");
+                d3.select(".fashionDiv").classed("displayed", true).classed("hidden", false);
                 break;
             case "Film and Video":
-                d3.select(".filmDiv").style("display", "inline-block");
+                d3.select(".filmDiv").classed("displayed", true).classed("hidden", false);
                 break;
             case "Food":
-                d3.select(".foodDiv").style("display", "inline-block");
+                d3.select(".foodDiv").classed("displayed", true).classed("hidden", false);
                 break;
             case "Games":
-                d3.select(".gamesDiv").style("display", "inline-block");
+                d3.select(".gamesDiv").classed("displayed", true).classed("hidden", false);
                 break;
             case "Journalism":
-                d3.select(".journalDiv").style("display", "inline-block");
+                d3.select(".journalDiv").classed("displayed", true).classed("hidden", false);
                 break;
             case "Music":
-                d3.select(".musicDiv").style("display", "inline-block");
+                d3.select(".musicDiv").classed("displayed", true).classed("hidden", false);
                 break;
             case "Photography":
-                d3.select(".photoDiv").style("display", "inline-block");
+                d3.select(".photoDiv").classed("displayed", true).classed("hidden", false);
                 break;
             case "Publishing":
-                d3.select(".publishDiv").style("display", "inline-block");
+                d3.select(".publishDiv").classed("displayed", true).classed("hidden", false);
                 break;
             case "Technology":
-                d3.select(".techDiv").style("display", "inline-block");
+                d3.select(".techDiv").classed("displayed", true).classed("hidden", false);
                 break;
             case "Theater":
-            d3.select(".theaterDiv").style("display", "inline-block");
+                d3.select(".theaterDiv").classed("displayed", true).classed("hidden", false);
                 break;
 
         }
@@ -57,12 +59,19 @@ $(document).ready(function() {
             xhr.open("POST", '/api/compute');
             xhr.send(new FormData(document.getElementById("asdf")));
             xhr.onload = () => {
-                alert(xhr.responseText);
+                d3.select("#result").text(xhr.responseText).classed("hidden", false).classed("displayed", true);
                 resolve(xhr.responseText);
             };
             xhr.error = () => reject();
         });
         return false;
+    });
+
+    document.getElementById("reset").addEventListener("click", () => {
+        d3.select("#result").classed("hidden", true).classed("displayed", false);
+        d3.selectAll(".displayed").classed("hidden", true).classed("displayed", false);
+        document.getElementById("goal_input").value = "";
+        document.getElementById("backer_input").value = "";
     });
 });
 
